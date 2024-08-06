57280549e5521e2479f52d255ec338cc597732a5
Elina Jaimes <evjaimes11@gmail.com>
2024-08-06 14:20:09 -0500

    :lock: feat: delete dist

--- dist/routes/user.api.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const user_controller_1 = require("../controllers/user.controller");
-const userRouter = (0, express_1.Router)();
-const userController = new user_controller_1.UserController();
-userRouter.get("/:pk", userController.getOne.bind(userController));
-userRouter.get("/", userController.getAll.bind(userController));
-exports.default = userRouter;
